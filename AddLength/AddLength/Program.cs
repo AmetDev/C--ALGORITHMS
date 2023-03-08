ffb6076db51effb5bca07b6c350204f9386b28f9
AmetJunior <ametsejdaliev@gmail.com>
2023-03-08 22:52:24 +0300

    AddLength

---
+++ AddLength/AddLength/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace AddLength
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] text = AddLength(str);
+            foreach (var VARIABLE in text)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+
+        public static string[] AddLength(string str)
+        {
+            string[] words = str.Split();
+            string[] wordsWithLength = new string[words.Length];
+            for (int i = 0; i < words.Length; i++)
+            {
+                wordsWithLength[i] = $"{words[i]} {words[i].Length}";
+            }
+           
+            return wordsWithLength;
+        }
+    }
+}
--- AddLength/AddLength/Properties/AssemblyInfo.cs
+++ AddLength/AddLength/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication6")]
+[assembly: AssemblyTitle("AddLength")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication6")]
+[assembly: AssemblyProduct("AddLength")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("D58A3AEE-82E9-41D4-86BA-D56DDAE91C26")]
+[assembly: Guid("596B2141-6DDF-40AC-A5D8-E92517AC73BF")]
 
 // Version information for an assembly consists of the following four values:
 //
