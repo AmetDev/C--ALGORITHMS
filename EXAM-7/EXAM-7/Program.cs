3200acb39b2f856d15efa01bcac05c2660a28f0b
AmetJunior <ametsejdaliev@gmail.com>
2023-06-11 14:24:15 +0300

    EXAM-7_1

---
+++ EXAM-7/EXAM-7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EXAM_7
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int Y;
+            if (A < B)
+            {
+                Y = A;
+                Console.WriteLine("A="+Y);
+            }
+            else
+            {
+                Y = B;
+                Console.WriteLine("B="+Y);
+            }
+
+           
+        }
+    }
+}
