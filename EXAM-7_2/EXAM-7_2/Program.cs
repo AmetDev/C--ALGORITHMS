952868f3ba83acc1e4f9510724ea1a5176a47398
AmetJunior <ametsejdaliev@gmail.com>
2023-06-11 14:35:48 +0300

    7.2

---
+++ EXAM-7_2/EXAM-7_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace EXAM_7_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            double y;
+            if (x < -10)
+            {
+                y = Math.Sqrt(x+12);
+                Console.WriteLine("x < -10="+y);
+            }
+            else if (x <= 10 && x <= -10)
+            {
+                y = 1/(x + 5);
+                Console.WriteLine("x <= 10 && x <= -10"+y);
+            }
+            else if (x>10)
+            {
+                y = 1 /( (11 - x) + 5);
+                Console.WriteLine("x>10"+y);
+            }
+
+        }
+    }
+}
